9b861b5acc77f238586ee993d28c3a7903a90ebd
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2020-01-29 12:38:48 -0500

    fix more styling for form

--- client/src/components/VendorSignUp/index.js
+++ client/src/components/VendorSignUp/index.js
@@ -71,37 +71,37 @@ class VendorSignUp extends React.Component {
 					<div className="col-md-6 mt-5 mx-auto">
 						<h1>Create account</h1>
 						<form className="form-group" onSubmit={this.onSubmit}>
+							<h2>Contact Information</h2>
 							<div className="form-group row">
-								<h2>Contact Information</h2>
-								<input placeholder="First Name" className="resturant-input" type="text" name="firstName" value={this.state.firstName} onChange={this.onChange} />
-								<input placeholder="Last Name" className="resturant-input" type="text" name="lastName" value={this.state.lastName} onChange={this.onChange} />
-								<input placeholder="Title:" className="resturant-input" type="text" name="title" value={this.state.title} onChange={this.onChange} />
-								<input placeholder="Email" className="resturant-input" type="text" name="email" value={this.state.email} onChange={this.onChange} />
-								<input placeholder="hone Number" className="resturant-input" type="text" name="phoneNumber" value={this.state.phoneNumber} onChange={this.onChange} />
+								<input placeholder="First Name" className="form-control col-md-6" type="text" name="firstName" value={this.state.firstName} onChange={this.onChange} />
+								<input placeholder="Last Name" className="form-control col-md-6" type="text" name="lastName" value={this.state.lastName} onChange={this.onChange} />
+								<input placeholder="Title:" className="form-control col-md-12" type="text" name="title" value={this.state.title} onChange={this.onChange} />
+								<input placeholder="Email" className="form-control col-md-12" type="text" name="email" value={this.state.email} onChange={this.onChange} />
+								<input placeholder="hone Number" className="form-control col-md-12" type="text" name="phoneNumber" value={this.state.phoneNumber} onChange={this.onChange} />
 							</div>
+							<h2> Nonprofit Information</h2>
 							<div className="form-group row">
-								<h2> Nonprofit Information</h2>
-								<input placeholder="Company Name" className="resturant-input" type="text" name="companyName" value={this.state.companyName} onChange={this.onChange} />
-								<input placeholder="Company Website" className="resturant-input" type="text" name="companyWebsite" value={this.state.companyWebsite} onChange={this.onChange} />
-								<input placeholder="EIN" className="resturant-input" type="text" name="ein" value={this.state.ein} onChange={this.onChange} />
+								<input placeholder="Company Name" className="form-control col-md-12" type="text" name="companyName" value={this.state.companyName} onChange={this.onChange} />
+								<input placeholder="Company Website" className="form-control col-md-12" type="text" name="companyWebsite" value={this.state.companyWebsite} onChange={this.onChange} />
+								<input placeholder="EIN" className="form-control col-md-12" type="text" name="ein" value={this.state.ein} onChange={this.onChange} />
 							</div>
+							<h3>Company Address:</h3>
 							<div className="form-group row">
-								<h3>Company Address:</h3>
-								<input placeholder="Company Address" className="address-input" type="text" name="companyAddress" value={this.state.companyAddress} onChange={this.onChange} />
-								<input placeholder="City" className="address-input" type="text" name="companyCity" value={this.state.companyCity} onChange={this.onChange} />
-								<input placeholder="State" className="address-input" type="text" name="companyState" value={this.state.companyState} onChange={this.onChange} />
-								<input placeholder="Zip" className="address-input" type="text" name="companyZip" value={this.state.companyZip} onChange={this.onChange} />
-								<input placeholder="Country" className="address-input" type="text" name="companyCountry" value={this.state.companyCountry} onChange={this.onChange} />
+								<input placeholder="Company Address" className="form-control col-md-12" type="text" name="companyAddress" value={this.state.companyAddress} onChange={this.onChange} />
+								<input placeholder="City" className="form-control col-md-12" type="text" name="companyCity" value={this.state.companyCity} onChange={this.onChange} />
+								<input placeholder="State" className="form-control col-md-12" type="text" name="companyState" value={this.state.companyState} onChange={this.onChange} />
+								<input placeholder="Zip" className="form-control col-md-12" type="text" name="companyZip" value={this.state.companyZip} onChange={this.onChange} />
+								<input placeholder="Country" className="form-control col-md-12" type="text" name="companyCountry" value={this.state.companyCountry} onChange={this.onChange} />
 							</div>
+							<h3>Will any items require refigeration?</h3>
 							<div className="form-group row">
-								<h3>Will any items require refigeration?</h3>
 								<select value={this.state.refigeration} onChange={this.onChange}>
 									<option value="yes">Yes</option>
 									<option value="no">No</option>
 								</select>
 							</div>
+							<h3>Do you have anyone on your team that can dropoff the food order?</h3>
 							<div className="form-group row">
-								<h3>Do you have anyone on your team that can dropoff the food order?</h3>
 								<select value={this.state.dropoff} onChange={this.onChange}>
 									<option value="yes">Yes</option>
 									<option value="no">No</option>
