089cf797557f0c60ebf49d5a9c5c689a8aafc4f0
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2020-01-29 12:11:03 -0500

    fix up domr dtyling on form page

--- client/src/components/NonprofitSignUp/index.js
+++ client/src/components/NonprofitSignUp/index.js
@@ -1,89 +1,57 @@
 import React from "react";
 
 class NonprofitSignUp extends React.Component {
-    render() {
-        return (
-            <div className="create_business_registration">
-               <div className="card-header bg-primary text-white">
-            <h1><strong>Create account</strong></h1>
-
-            <form> 
-            <h2>Contact Information</h2>
-               <label>
-                   First Name:
-                   <input type="text" name="firstName" value=""/>
-               </label>
-               
-               <label>
-                   Last Name:
-                   <input type="text" name="lastName" value=""/>
-               </label>
-               
-               <label>
-                   Title:
-               <input type="text" name="title" value=""/>
-               </label>
-               
-               <label>
-                   Email:
-                   <input type="text" name="email" value=""/>
-               </label>
-               
-               <label>
-                   Phone Number:
-                   <input type="text" name="phoneNumber" value=""/>
-               </label>
-              
-              <h3> Nonprofit Information</h3>
-               <label>
-                   Company Name:
-                   <input type="text" name="companyName" value=""/>
-               </label>
-               
-               <label>
-                   Company Website:
-               <input type="text" name="companyWebsite" value="" />
-               </label>
-               
-               <label>
-                   Company Address:
-                   <input type="text" name="street" value="" />
-                   <input type="text" name="city" value="" />
-                   <input type="text" name="state" value="" />
-                   <input type="text" name="zip" value="" />
-                   <input type="text" name="country" value="" />
-               </label>
-               <label>
-                   Do you have anyone on your team that can pickup the food order?
-                   <select>
-                    <option value="yes">Yes</option>
-                    <option value="no">No</option>
-                   </select>
-               </label>
-               <div>
-                   What types of food do you need for your orgainzation?
-                   
-                   <input type="checkbox" id="produce" name="produce"/>
-                    <input type="checkbox" id="dairy" name="dairy"/>
-                    <input type="checkbox" id="meat" name="meat"/>
-                    <input type="checkbox" id="grain" name="grain"/>
-                    <input type="checkbox" id="cannedItems" name="cannedItems"/>
-                    <input type="checkbox" id="boxedItems" name="boxedItems"/>
-                    <input type="checkbox" id="other" name="other"/>
-               </div>
-               
-           </form>  
-               <button 
-                 type="submit" 
-                 className="button button_wide">
-                 CREATE ACCOUNT
-               </button>
-         
-           </div> 
-           </div>
-         );
-       
-   }
+	render() {
+		return (
+			<div className="container create-business-registration">
+			   <div className="row"></div>
+				<div className="col-md-6 mt-5 mx-auto ">
+					<h1>Create Account</h1>
+					<form> 
+						<h2>Contact Information</h2>
+						<input placeholder="First Name" type="text" name="firstName" value=""/>
+						<input placeholder="Last Name" type="text" name="lastName" value=""/>
+						<input placeholder="Title" type="text" name="title" value=""/>
+						<input placeholder="Email" type="text" name="email" value=""/>
+						<input placeholder="Phone Number" type="text" name="phoneNumber" value=""/>
+				
+						<h2> Nonprofit Information</h2>
+						<input placeholder="Company Name" type="text" name="companyName" value=""/> 
+						<input placeholder="Web Site" type="text" name="companyWebsite" value="" />
+				
+						<h3>Company Address</h3>
+						<input placeholder="Street" type="text" name="street" value="" />
+						<input placeholder="City" type="text" name="city" value="" />
+						<input placeholder="State" type="text" name="state" value="" />
+						<input placeholder="Zip" type="text" name="zip" value="" />
+						<input placeholder="Country" type="text" name="country" value="" />
+					
+						<h3>>Do you have anyone on your team that can pickup the food order?</h3>
+						<select>
+								<option value="yes">Yes</option>
+								<option value="no">No</option>
+						</select>
+				
+						types of food do you need for your orgainzation?
+				   
+				   		<input placeholder="" type="checkbox" id="produce" name="produce"/>
+						<input placeholder="" type="checkbox" id="dairy" name="dairy"/>
+						<input placeholder="" type="checkbox" id="meat" name="meat"/>
+						<input placeholder="" type="checkbox" id="grain" name="grain"/>
+						<input placeholder="" type="checkbox" id="cannedItems" name="cannedItems"/>
+						<input placeholder="" type="checkbox" id="boxedItems" name="boxedItems"/>
+						<input placeholder="" type="checkbox" id="other" name="other"/>
+ 
+					</form>  
+					<button 
+						placeholder="" type="submit" 
+						className="button button_wide">
+						CREATE ACCOUNT
+					</button>
+		   		</div> 
+		   </div>
+		 );   
+	}
 }
-     
+	 
 export default NonprofitSignUp;
--- client/src/components/VendorSignUp/index.js
+++ client/src/components/VendorSignUp/index.js
@@ -64,86 +64,71 @@ class VendorSignUp extends React.Component {
             })
     }
 
-
-
-    render() {
-        return (
-            <div className="create_business_registration">
-                <div className="card-header bg-primary text-white">
-                    <h1>Create account</h1>
-
-                    <form className="form-group" onSubmit={this.onSubmit}>
-                        <h2>Contact Information</h2>
-                        <div>
-                            <label>First Name:</label>
-                            <input className="resturant-input" type="text" name="firstName" value={this.state.firstName} onChange={this.onChange} />
-                        </div>
-                        <div>
-                            <label>Last Name:</label>
-                            <input className="resturant-input" type="text" name="lastName" value={this.state.lastName} onChange={this.onChange} />
-                        </div>
-                        <div>
-                            <label>Title:</label>
-                            <input className="resturant-input" type="text" name="title" value={this.state.title} onChange={this.onChange} />
-                        </div>
-                        <div>
-                            <label htmlFor="email">Email:</label>
-                            <input className="resturant-input" type="text" name="email" value={this.state.email} onChange={this.onChange} />
-                        </div>
-                        <div>
-                            <label htmlFor="phoneNumber">Phone Number:</label>
-                            <input className="resturant-input" type="text" name="phoneNumber" value={this.state.phoneNumber} onChange={this.onChange} />
-                        </div>
-                        <h3> Nonprofit Information</h3>
-                        <div>
-                            <label htmlFor="companyName">Company Name:</label>
-                            <input className="resturant-input" type="text" name="companyName" value={this.state.companyName} onChange={this.onChange} />
-                        </div>
-                        <div>
-                            <label>Company Website:</label>
-                            <input className="resturant-input" type="text" name="companyWebsite" value={this.state.companyWebsite} onChange={this.onChange} />
-                        </div>
-                        <div>
-                            <label>Password:</label>
-                            <input className="resturant-input" type="text" name="password" value={this.state.password} onChange={this.onChange} />
-                        </div>
-                        <div>
-                            <label>EIN:</label>
-                            <input className="resturant-input" type="text" name="ein" value={this.state.ein} onChange={this.onChange} />
-                        </div>
-                        <div>
-                            <label>Company Address:</label>
-                            <input className="address-input" type="text" name="companyAddress" value={this.state.companyAddress} onChange={this.onChange} />
-                            <input className="address-input" type="text" name="companyCity" value={this.state.companyCity} onChange={this.onChange} />
-                            <input className="address-input" type="text" name="companyState" value={this.state.companyState} onChange={this.onChange} />
-                            <input className="address-input" type="text" name="companyZip" value={this.state.companyZip} onChange={this.onChange} />
-                            <input className="address-input" type="text" name="companyCountry" value={this.state.companyCountry} onChange={this.onChange} />
-                        </div>
-                        <div>
-                            <label>Will any items require refigeration?</label>
-                            <select value={this.state.refigeration} onChange={this.onChange}>
-                                <option value="yes">Yes</option>
-                                <option value="no">No</option>
-                            </select>
-                        </div>
-                        <div className="drop-off-section">
-                            <label>Do you have anyone on your team that can dropoff the food order?</label>
-                            <select value={this.state.dropoff} onChange={this.onChange}>
-                                <option value="yes">Yes</option>
-                                <option value="no">No</option>
-                            </select>
-                        </div>
-                        <button
-                            type="submit"
-                            className="button button_wide">
-                            CREATE ACCOUNT
-                </button>
-                    </form>
-
-                </div>
+	render() {
+		return (
+        	<div className="container">
+				<div className="row">
+					<div className="card-header bg-primary text-white">
+						<h1>Create account</h1>
+						<form className="form-group" onSubmit={this.onSubmit}>
+							<h2>Contact Information</h2>
+							<div>
+								<input placeholder="First Name" className="resturant-input" type="text" name="firstName" value={this.state.firstName} onChange={this.onChange} />
+							</div>
+							<div>
+								<input placeholder="Last Name" className="resturant-input" type="text" name="lastName" value={this.state.lastName} onChange={this.onChange} />
+							</div>
+							<div>
+								<input placeholder="Title:" className="resturant-input" type="text" name="title" value={this.state.title} onChange={this.onChange} />
+							</div>
+							<div>
+								<input placeholder="Email" className="resturant-input" type="text" name="email" value={this.state.email} onChange={this.onChange} />
+							</div>
+							<div>
+								<input placeholder="hone Number" className="resturant-input" type="text" name="phoneNumber" value={this.state.phoneNumber} onChange={this.onChange} />
+							</div>
+							<h2> Nonprofit Information</h2>
+							<div>
+								<input placeholder="Company Name" className="resturant-input" type="text" name="companyName" value={this.state.companyName} onChange={this.onChange} />
+							</div>
+							<div>
+								<input placeholder="Company Website" className="resturant-input" type="text" name="companyWebsite" value={this.state.companyWebsite} onChange={this.onChange} />
+							</div>
+							<div>
+								<input placeholder="EIN" className="resturant-input" type="text" name="ein" value={this.state.ein} onChange={this.onChange} />
+							</div>
+							<div>
+								<h3>Company Address:</h3>
+								<input placeholder="Company Address" className="address-input" type="text" name="companyAddress" value={this.state.companyAddress} onChange={this.onChange} />
+								<input placeholder="City" className="address-input" type="text" name="companyCity" value={this.state.companyCity} onChange={this.onChange} />
+								<input placeholder="State" className="address-input" type="text" name="companyState" value={this.state.companyState} onChange={this.onChange} />
+								<input placeholder="Zip" className="address-input" type="text" name="companyZip" value={this.state.companyZip} onChange={this.onChange} />
+								<input placeholder="Country" className="address-input" type="text" name="companyCountry" value={this.state.companyCountry} onChange={this.onChange} />
+							</div>
+							<div>
+								<h3>Will any items require refigeration?</h3>
+								<select value={this.state.refigeration} onChange={this.onChange}>
+									<option value="yes">Yes</option>
+									<option value="no">No</option>
+								</select>
+							</div>
+							<div className="drop-off-section">
+								<h3>Do you have anyone on your team that can dropoff the food order?</h3>
+								<select value={this.state.dropoff} onChange={this.onChange}>
+									<option value="yes">Yes</option>
+									<option value="no">No</option>
+								</select>
+							</div>
+							<button
+								type="submit"
+								className="button button_wide">
+								CREATE ACCOUNT
+					</button>
+						</form>
+					</div>
+				</div>
             </div>
         );
-
     }
 }
 
