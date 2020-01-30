c0d2ed3a261dd6014873da40b047b229638ae6a2
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2020-01-29 21:02:12 -0500

    update create account button

--- client/src/components/CreateAccountButton/index.js
+++ client/src/components/CreateAccountButton/index.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function CreateAccountButton() {
     return (
-        <div class="container">
-            <a class="btn-green btn btn-lg btn-block" href="/user-registration">Create An Account</a>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 mt-5 mx-auto">
+                    <a className="btn-green btn btn-lg btn-block" href="/user-registration">Create An Account</a>
+                </div>
+            </div>
         </div>
     );
 }
